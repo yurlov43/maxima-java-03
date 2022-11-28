83f72361a2a90826ba1c7dcefbfed7f719c71d1b
Vladimir Yurlov <yurlov43@gmail.com>
2022-11-28 21:23:06 +0300

    Removed constructors of Queue Kitchen and StackKitchen classes

--- src/main/java/org/example/QueueKitchen.java
+++ src/main/java/org/example/QueueKitchen.java
@@ -2,10 +2,6 @@ package org.example;
 
 public class QueueKitchen<T> extends Kitchen<T> {
 
-    public QueueKitchen() {
-        super();
-    }
-
     public void feed() {
         int length = getAnimals().size();
         if (length > 0) getAnimals().remove(0);
--- src/main/java/org/example/StackKitchen.java
+++ src/main/java/org/example/StackKitchen.java
@@ -2,10 +2,6 @@ package org.example;
 
 public class StackKitchen<T> extends Kitchen<T> {
 
-    public StackKitchen() {
-        super();
-    }
-
     public void feed() {
         int length = getAnimals().size();
         if (length > 0) getAnimals().remove(length - 1);
